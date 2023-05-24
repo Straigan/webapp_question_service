213e995bf35239534e25790b2b3043d0a287123b
Ilya Styapin <istyapin@gmail.com>
2023-05-25 00:02:12 +0500

    edit __init__.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,10 +10,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
-    migrate = Migrate(app, db)
+    Migrate(app, db)
     app.register_blueprint(question_service_blueprint)
     with app.app_context():
         db.create_all()
-    Migrate(app, db)
 
     return app
